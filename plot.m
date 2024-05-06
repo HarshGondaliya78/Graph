9e19ea479bc58936ea58fe7d62101cbc52a70aac
Harsh Gondaliya <157871191+HarshGondaliya78@users.noreply.github.com>
2024-05-06 10:03:54 +0530

    plot.m

--- plot.m
+++ plot.m
@@ -1,10 +1,8 @@
 t=linspace(-2,2,200);
-
 f=100;
 
-%figure(1);
+%define x and y values
 x=3*sin(2*pi*f*t); 
-
 y=3*cos(2*pi*f*t);
 
 % plot(x,'--rs','LineWidth',n,...)
@@ -13,11 +11,11 @@ subplot(3,1,1)
 plot(t,x,'r','LineWidth',2);
 hold on;
 
-grid off;
-xlabel('time')
-ylabel('amplitude')
-title('plot of sine wave')
-legend('sin wave')
+ grid off;
+ xlabel('time')
+ ylabel('amplitude')
+ title('plot of sine wave')
+ legend('sin wave')
  
 % plot(y,'--rs','LineWidth',n,...)
 % figure(2);
@@ -25,11 +23,11 @@ subplot(3,1,2)
 plot(t,y,'b','LineWidth',2);
 hold on;
 
-grid off;
-xlabel('time')
-ylabel('amplitude')
-title('plot of cos wave')
-legend('cos wave')
+ grid off;
+ xlabel('time')
+ ylabel('amplitude')
+ title('plot of cos wave')
+ legend('cos wave')
 
 % plot(x,y,'--rs','LineWidth',n,...)
 % figure(3);
@@ -38,8 +36,8 @@ plot(t,x,'--r','LineWidth',2);
 hold on;
 plot(t,y,'b','LineWidth',2);
 
-grid on;
-xlabel('time')
-ylabel('amplitude')
-title('plot of sine and cos wave')
-legend('sin wave ','cos wave')
+ grid on;
+ xlabel('time')
+ ylabel('amplitude')
+ title('plot of sine and cos wave')
+ legend('sin wave ','cos wave')
